5dbe03e613174ab68c16e6f624154bf6b0dd1178
Patchawit Muensoontorn <42910396+Patchawit@users.noreply.github.com>
2018-12-19 19:00:07 +0700

    Add files via upload

---
+++ Graph/April.py
@@ -0,0 +1,14 @@
+"""carsales"""
+import pandas as pd
+import matplotlib.pyplot as plt
+def main():
+    """carsalespermonth"""
+    df = pd.read_csv('car.csv')
+    df.set_index("Brand", inplace=True)
+    ax = df["April"].plot(kind="bar", figsize=(10, 10));
+    ax.set_title('ยอดขายรถยนต์ในเดือนเมษายน',fontname='Circular',fontsize='22')
+    ax.set_ylabel("จำนวนยอดขายรถยนต์(คัน)", fontname='Circular', fontsize='20')
+    ax.set_xlabel("ยี่ห้อรถยนต์", fontname='Circular', fontsize='20')
+    plt.show()
+
+main()
---
+++ Graph/September.py
@@ -0,0 +1,14 @@
+"""carsales"""
+import pandas as pd
+import matplotlib.pyplot as plt
+def main():
+    """carsalespermonth"""
+    df = pd.read_csv('car.csv')
+    df.set_index("Brand", inplace=True)
+    ax = df["September"].plot(kind="bar", figsize=(10, 10));
+    ax.set_title('ยอดขายรถยนต์ในเดือนกันยายน',fontname='Circular',fontsize='22')
+    ax.set_ylabel("จำนวนยอดขายรถยนต์(คัน)", fontname='Circular', fontsize='20')
+    ax.set_xlabel("ยี่ห้อรถยนต์", fontname='Circular', fontsize='20')
+    plt.show()
+
+main()
